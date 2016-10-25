03a0b46ae27599a5556b2b585a6967a2dadddb98
Roman <roman.pawliw@gmail.com>
2016-10-25 12:54:29 +0300

    Add files via upload

--- app_client/modalForm/uibModalController.js
+++ app_client/modalForm/uibModalController.js
@@ -1,30 +1,30 @@
 (function () {
 
 angular
-   .module('app')
-   .controller('modalCtrl', modalCtrl);
+  .module('app')
+  .controller('modalCtrl', modalCtrl);
 
 modalCtrl.$inject = ['$scope', '$uibModalInstance', 'todoData'];
 
 function modalCtrl ($scope, $uibModalInstance, todoData) {
+	$scope.formError = "";
 
 	$scope.onSubmit = function () {
 
-		$scope.formError = "more than 15";
-
 		if ($scope.formData.title.length > 15) {
-		  	$scope.formError = 'more than 15';
+		  	$scope.formError = 'more than 15 characters';
 		  	return false;
 		}
-
-		todoData.recordCreate($scope.formData)
-		  .success(function(data) {
-            $scope.close();
-		  })
-          .error(function(data) {
-          	$scope.formError = "Your review has not been saved, please try again";
-          });
-		return false;
+		else {
+			todoData.recordCreate($scope.formData)
+			  .success(function(data) {
+	            $scope.close();
+			  })
+	          .error(function(data) {
+	          	$scope.formError = "Your review has not been saved, please try again";
+	          });
+			return false;
+		}
 	};
 
 	$scope.close = function () {
