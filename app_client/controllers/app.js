ab95c86e64bd60500b249b2a19cb6d4cbd012a89
Roman <roman.pawliw@gmail.com>
2016-10-28 13:22:15 +0300

    Delete app.js

--- app_client/controllers/app.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('app', ['ngRoute']);
-
-app.controller('homeCtrl', function($scope, $compile, $element) {
-	$scope.createTask = function() {
-		var ul = $element.find('ul');
-		console.log(ul);
-	}
-});
